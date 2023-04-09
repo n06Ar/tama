d14a2dc6d148811682dde2763b9ce50e272740ff
Nobuhito ARAI <nobuhito.arai@n06ar.me>
2023-04-09 18:13:05 +0900

    ♻️change call instance.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,17 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import TMClient from "./client/TMClient";
 
 dotenv.config();
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+const mtClient = new TMClient();
 
-const TOKEN: string = process.env.DISCORD_TOKEN;
+mtClient
+  .addMessageResponse([
+    {
+      targetMessage: "pong",
+      responseMessage: "Ping!",
+    },
+  ])
+  .finally();
 
-client.once(Events.ClientReady, () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on(Events.MessageCreate, async (message) => {
-  if (message.author.bot) return;
-
-  if (message.content === "ping") {
-    await message.channel.send("Pong!");
-  }
-});
-
-client.login(TOKEN).catch((e) => {
-  console.log({ e });
-});
+mtClient.start().finally();
